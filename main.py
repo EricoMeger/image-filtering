c5299328cf437ed3c7caff4b98fb9d15be7cea4c
EricoMeger <ericomeger9@gmail.com>
2024-12-05 18:59:57 -0300

    change variable names

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import cv2
 
 def Mean3x3Filter(img):
-    image_matrix = img.copy()
-    for i in range(1, image_matrix.shape[0] - 1):
-        for j in range(1, image_matrix.shape[1] - 1):
-            mean = (image_matrix[i-1, j-1] + image_matrix[i-1, j] + image_matrix[i-1, j+1] + image_matrix[i, j-1] + image_matrix[i, j] + image_matrix[i, j+1] + image_matrix[i+1, j-1] + image_matrix[i+1, j] + image_matrix[i+1, j+1]) / 9
-            image_matrix[i, j] = mean
+    img_filtered = img.copy()
+    for i in range(1, img_filtered.shape[0] - 1):
+        for j in range(1, img_filtered.shape[1] - 1):
+            mean = (img_filtered[i-1, j-1] + img_filtered[i-1, j] + img_filtered[i-1, j+1] + img_filtered[i, j-1] + img_filtered[i, j] + img_filtered[i, j+1] + img_filtered[i+1, j-1] + img_filtered[i+1, j] + img_filtered[i+1, j+1]) / 9
+            img_filtered[i, j] = mean
 
-    return image_matrix
+    return img_filtered
+
+def Median3x3Filter(img):
+    img_filtered = img.copy()
+
+    return img_filtered
 
 
 path = "./image.jpg"
