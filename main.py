fb0ddb908337050b63387b6b52a44c256aa6a948
EricoMeger <ericomeger9@gmail.com>
2024-12-05 21:24:14 -0300

    fix runtime overflow

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def Mean3x3Filter(img):
     img_filtered = img.copy()
     for i in range(1, img_filtered.shape[0] - 1):
         for j in range(1, img_filtered.shape[1] - 1):
-            mean = (img_filtered[i-1, j-1] + img_filtered[i-1, j] + img_filtered[i-1, j+1] + img_filtered[i, j-1] + img_filtered[i, j] + img_filtered[i, j+1] + img_filtered[i+1, j-1] + img_filtered[i+1, j] + img_filtered[i+1, j+1]) // 9
-            img_filtered[i, j] = mean
+            mean = (int(img_filtered[i-1, j-1]) + int(img_filtered[i-1, j]) + int(img_filtered[i-1, j+1]) + 
+                    int(img_filtered[i, j-1]) + int(img_filtered[i, j]) + int(img_filtered[i, j+1]) + 
+                    int(img_filtered[i+1, j-1]) + int(img_filtered[i+1, j]) + int(img_filtered[i+1, j+1])) // 9
+            
+            img_filtered[i, j] = min(mean, 255)
 
     #Tratar borda
     return img_filtered
